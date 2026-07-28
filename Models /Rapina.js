d6beb7db70a6f54deed58d18da6552a2d1e4cab8
JustxMegh <saragaldo20@gmail.com>
2026-07-29 01:41:59 +0200

    Update taggedUsers field to be optional
    Removed required constraint from taggedUsers field in rapinaSchema.

--- Models /Rapina.js
+++ Models /Rapina.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const rapinaSchema = new mongoose.Schema({
   executorId: { type: String, required: true },
-  taggedUsers: [{ type: String, required: true }],
+  taggedUsers: [{ type: String }],
   totalAmount: { type: Number, required: true },
   splitAmountPerUser: { type: Number, required: true },
   createdAt: { type: Date, default: Date.now }
