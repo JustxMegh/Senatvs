e7e972c37497a39e044d98e07347dd19de3daf71
JustxMegh <saragaldo20@gmail.com>
2026-07-29 16:06:18 +0200

    Add files via upload

---
+++ discord.js
@@ -0,0 +1,18 @@
+const { REST, Routes } = require('discord.js');
+
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+(async () => {
+  try {
+    console.log('Registering slash commands...');
+
+    await rest.put(
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      { body: commandsArray }, // Replace 'commandsArray' with your commands array/data
+    );
+
+    console.log('Slash commands registered successfully!');
+  } catch (error) {
+    console.error(error);
+  }
+})();
