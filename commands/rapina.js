1806d795fed5f18ab6d617290fdb47edecf5893f
JustxMegh <saragaldo20@gmail.com>
2026-07-29 02:23:36 +0200

    Add rapina command to log heists
    This command allows users to log a new heist with a specified total amount, saving the record to the MongoDB database.

---
+++ commands/rapina.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require('discord.js');
+const Rapina = require('../Models/Rapina.js'); // Import from /Models
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('rapina')
+    .setDescription('Log a new heist')
+    .addIntegerOption(option =>
+      option.setName('totale')
+        .setDescription('Total heist amount')
+        .setRequired(true)),
+
+  async execute(interaction) {
+    await interaction.deferReply();
+    const totale = interaction.options.getInteger('totale');
+
+    // Create a new record in MongoDB using the Rapina model
+    const newRapina = new Rapina({
+      userId: interaction.user.id,
+      amount: totale,
+      date: new Date(),
+    });
+
+    await newRapina.save();
+
+    await interaction.editReply({
+      content: `💰 Rapina of **$${totale}** saved to database!`,
+    });
+  },
+};
