5961980d8af4ac1cad112178129e33d131d60bee
JustxMegh <saragaldo20@gmail.com>
2026-07-29 16:02:37 +0200

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-  token: process.env.DISCORD_TOKEN,
-  clientId: process.env.CLIENT_ID,
-  devGuildId: process.env.DEV_GUILD_ID || null,
-  mongoUri: process.env.MONGODB_URI,
-};
